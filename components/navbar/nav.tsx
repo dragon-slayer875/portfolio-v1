04ece3c289be1bd6fe2807d2579248db992c8570
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-01-10 20:36:08 +0530

    update navbar styles

--- components/navbar/nav.tsx
+++ components/navbar/nav.tsx
@@ -20,18 +20,18 @@ const Nav = (NavProps: any) => {
     return (
         <nav
             className={cn(
-                "fixed z-50 top-0 p-5 w-[100dvw] flex justify-between items-center"
+                "fixed backdrop-blur-md z-50 top-0 p-5 w-dvw flex justify-between items-center shadow-sm shadow-primary transition-300",
             )}
         >
             <div>
                 <h1 className="font-black text-4xl sm:text-4xl">Rt</h1>
             </div>
-            <div className="hidden md:grid grid-flow-col grid-cols-5 justify-items-center jus items-center gap-1">
+            <div className="hidden md:grid grid-flow-col grid-cols-5 justify-items-center jus items-center gap-3">
                 {links.map((link, index) => (
                     <Link
                         key={index}
                         href={link.link}
-                        className={`font-medium w-full px-3 mx-1 hover:shadow-[0_0_0_0.1rem_hsl(var(primary))] shadow-primary text-lg text-center rounded-3xl transition-300
+                        className={`font-medium w-full py-1 px-3 mx-1 hover:shadow-[0_0_0_0.1rem_hsl(var(primary))] shadow-primary text-lg text-center rounded-3xl transition-300
                         ${
                             NavProps.isVis === link.name
                                 ? "bg-primary text-background"
