954ce2e60e5ce6026b5f4b10fc9abfcffc5258c3
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-01-10 22:14:25 +0530

    chnage index layout

--- sections/index.tsx
+++ sections/index.tsx
@@ -1,46 +1,41 @@
 import Image from "next/image";
 import React, { forwardRef } from "react";
+import { ArrowDown } from "lucide-react";
 
 const Index = forwardRef(function (props, ref) {
     return (
-        <>
-            <div
-                ref={ref as React.RefObject<HTMLDivElement> | null}
-                id="Home"
-                // style={{
-                //     height: "100dvh",
-                //     width: "100vw",
-                // }}
-                className="flex flex-col min-h-screen min-w-dvw md:max-w-[80dvw] md:flex md:flex-row-reverse justify-center items-center gap-4"
-            >
-                <div
-                    style={{
-                        position: "relative",
-                        width: "45dvw",
-                        height: "auto",
-                        aspectRatio: "1/1",
-                        boxShadow: "-1.5rem 1.5rem 0 0",
-                        margin: "0 0 3rem 0",
-                    }}
-                >
-                    <Image
-                        fill
-                        objectFit="cover"
-                        src="/portrait.png"
-                        alt="Portrait, Rudraksh Tyagi"
-                    />
+        <section ref={ref as React.RefObject<HTMLDivElement> | null} id="Home"
+        className="py-12 xl:py-24 h-[84vh]">
+            <div className="container mx-auto">
+                <div className="flex flex-col items-center justify-between w-full">
+                    <div>
+                        <h1 className="font-bold text-4xl text-foreground">
+                            Rudraksh Tyagi
+                        </h1>
+                        <h2 className="text-2xl text-foreground">
+                            Software Engineer
+                        </h2>
+                    </div>
+                    <div
+                        style={{
+                            position: "relative",
+                            width: "45dvw",
+                            height: "auto",
+                            aspectRatio: "1/1",
+                            boxShadow: "-1.5rem 1.5rem 0 0",
+                            margin: "0 0 3rem 0",
+                        }}
+                    >
+                        <Image
+                            fill
+                            objectFit="cover"
+                            src="/portrait.png"
+                            alt="Portrait, Rudraksh Tyagi"
+                        />
+                    </div>
                 </div>
-                <div className="flex flex-col items-center justify-center w-full">
-                    <h1 className="font-bold text-4xl text-foreground">
-                        Rudraksh Tyagi
-                    </h1>
-                    <h2 className="text-2xl text-foreground">
-                        Software Engineer
-                    </h2>
-                </div>
-
             </div>
-        </>
+        </section>
     );
 });
 
