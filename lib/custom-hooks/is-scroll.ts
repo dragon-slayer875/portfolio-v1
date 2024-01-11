c5acdbf237d3c8d38d7f3fae01260c1f4605ec1e
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-01-12 00:56:16 +0530

    create custom hook to check page scroll

---
+++ lib/custom-hooks/is-scroll.ts
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from "react";
+
+export default function useIsScroll() {
+    const [isScroll, setIsScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScroll(true);
+            } else {
+                setIsScroll(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return isScroll;
+}
