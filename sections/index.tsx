1330f43d2a7ed3b1a1f8248581d9271163e335ad
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-01-11 10:10:59 +0530

    update index section

--- sections/index.tsx
+++ sections/index.tsx
@@ -1,37 +1,37 @@
 import Image from "next/image";
-import React, { forwardRef } from "react";
-import { ArrowDown } from "lucide-react";
+import React from "react";
+import { forwardRef } from "react";
 
-const Index = forwardRef(function (props, ref) {
+const Index = forwardRef(function (_props, ref) {
     return (
-        <section ref={ref as React.RefObject<HTMLDivElement> | null} id="Home"
-        className="py-12 xl:py-24 h-[84vh]">
+        <section
+            ref={ref as React.RefObject<HTMLDivElement> | null}
+            id="Home"
+            className="p-12 lg:py-24 h-dvh flex items-center"
+        >
             <div className="container mx-auto">
-                <div className="flex flex-col items-center justify-between w-full">
-                    <div>
-                        <h1 className="font-bold text-4xl text-foreground">
-                            Rudraksh Tyagi
+                <div className="flex flex-col lg:!flex-row-reverse justify-between items-center gap-14">
+                    <Image
+                        width={400}
+                        height={400}
+                        src="/portrait.png"
+                        alt="Portrait, Rudraksh Tyagi"
+                        className="object-cover w-45dvw h-auto aspect-square shadow-[-1.5rem_1.5rem_0_0_hsl(var(--primary))]"
+                    />
+                    <div className="text-center flex flex-col gap-3 lg:text-left lg:max-w-[50dvw]">
+                        <div className=" text-2xl text-foreground">
+                            SOFTWARE ENGINEER
+                        </div>
+                        <h1 className="font-bold text-5xl text-foreground">
+                            Hello, my name is Rudraksh Tyagi
                         </h1>
-                        <h2 className="text-2xl text-foreground">
-                            Software Engineer
-                        </h2>
-                    </div>
-                    <div
-                        style={{
-                            position: "relative",
-                            width: "45dvw",
-                            height: "auto",
-                            aspectRatio: "1/1",
-                            boxShadow: "-1.5rem 1.5rem 0 0",
-                            margin: "0 0 3rem 0",
-                        }}
-                    >
-                        <Image
-                            fill
-                            objectFit="cover"
-                            src="/portrait.png"
-                            alt="Portrait, Rudraksh Tyagi"
-                        />
+                        <p className="text-lg">
+                            I am a software engineer based in India. I enjoy
+                            building things that live on the internet. I develop
+                            exceptional websites and web apps that provide
+                            intuitive, pixel-perfect user interfaces with
+                            efficient and modern backends.
+                        </p>
                     </div>
                 </div>
             </div>
