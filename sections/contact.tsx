71738d47018b83a2a041be9e749313f42623be08
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-01-13 15:25:48 +0530

    add socials

--- sections/contact.tsx
+++ sections/contact.tsx
@@ -1,7 +1,9 @@
 import React, { forwardRef } from "react";
 import { ContactForm } from "@/components/contact-form";
-import { Hand } from "lucide-react";
+import { Hand, Instagram, Linkedin } from "lucide-react";
+
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 const Contact = forwardRef(function (props, ref) {
     return (
@@ -11,14 +13,27 @@ const Contact = forwardRef(function (props, ref) {
             className="py-12 md:p-24 bg-secondary items-center"
         >
             <div className="container mx-auto ">
-                <div className="grid lg:grid-flow-col lg:grid-cols-3 items-center justify-items-center gap-4">
-                    <h1 className="items-center text-4xl font-bold text-foreground flex gap-3">
-                        Let's Talk
-                        <Hand size={30} className="animate-wiggle"></Hand>
-                    </h1>
+                <div className="grid gap-6 lg:gap-0 lg:grid-flow-col lg:grid-cols-3 items-center justify-items-center">
+                    <div className="w-full grid grid-rows-2 items-center justify-items-center justify-stretch">
+                        <h1 className="items-center text-4xl font-bold text-foreground flex gap-3">
+                            Let's Talk
+                            <Hand size={30} className="animate-wiggle"></Hand>
+                        </h1>
+                        <Separator className=" basis-full bg-foreground shrink-0 grow" />
+                        <div className="flex gap-4">
+                            <Link href="https://www.instagram.com/rudrakshtyagi875_"
+                            target="_blank">
+                                <Instagram size={30} />
+                            </Link>
+                            <Link href="https://www.linkedin.com/in/rudrakshtyagi875/"
+                            target="_blank">
+                                <Linkedin size={30} />
+                            </Link>
+                        </div>
+                    </div>
                     <Separator
-                    orientation="vertical"
-                       className="mx-4 h-full flex-1 bg-primary"
+                        orientation="vertical"
+                        className=" h-full flex-1 bg-primary "
                     />
                     <div className="w-full">
                         <ContactForm />
