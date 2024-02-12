8fb5fbbd7191b7c1da7752af93939e8a87894432
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-02-12 12:48:26 +0530

    improve logs

--- app/api/contact2/route.ts
+++ app/api/contact2/route.ts
@@ -1,34 +1,41 @@
-import { EmailTemplate } from '@/components/email-template';
-import { Resend } from 'resend';
-import * as React from 'react';
+import { EmailTemplate } from "@/components/email-template";
+import { Resend } from "resend";
+import * as React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-  try {
-    const mail_vals: {
-        from_name: string;
-        reply_to: string;
-        message: string;
+    try {
+        const mail_vals: {
+            from_name: string;
+            reply_to: string;
+            message: string;
         } = await request.json();
-    const { data, error } = await resend.emails.send({
-      from: 'Portfolio <onboarding@resend.dev>',
-      to: ['rudraksh875@gmail.com'],
-      subject: `Hello from ${mail_vals.from_name}`,
-      react: EmailTemplate({name: mail_vals.from_name, email: mail_vals.reply_to, message: mail_vals.message}) as React.ReactElement,
-    });
+        const { data, error } = await resend.emails.send({
+            from: "Portfolio <onboarding@resend.dev>",
+            to: ["rudraksh875@gmail.com"],
+            subject: `Hello from ${mail_vals.from_name}`,
+            react: EmailTemplate({
+                name: mail_vals.from_name,
+                email: mail_vals.reply_to,
+                message: mail_vals.message,
+            }) as React.ReactElement,
+        });
 
-    if (error) {
+        if (error) {
+            console.log(error);
+            console.log(process.env.RESEND_API_KEY);
+
+            return Response.json({ error });
+        }
+        console.log(process.env.RESEND_API_KEY);
+        console.log(data);
+
+        return Response.json({ data });
+    } catch (error) {
         console.log(error);
-        
-      return Response.json({ error });
+        console.log(process.env.RESEND_API_KEY);
+
+        return Response.json({ error });
     }
-    console.log(data);
-    
-    return Response.json({ data });
-  } catch (error) {
-    console.log(error);
-    
-    return Response.json({ error });
-  }
 }
